a87cec0a639c37a6926e26b27d2b3983b0994811
Alanjonesab <alanjones_18@hotmail.com>
2020-08-26 19:18:39 -0300

    Programa de saques e depósitos com tratamento de Exceções

---
+++ BankWithExceptions/Entities/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankWithExceptions.Entities.Exceptions;
+
+namespace BankWithExceptions.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new DomainException("Amount must be greater than zero");
+            }
+
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough Balance");
+            }
+            else if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+        }
+    }
+}
---
+++ BankWithExceptions/Program.cs
@@ -0,0 +1,65 @@
+﻿using BankWithExceptions.Entities;
+using BankWithExceptions.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace BankWithExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial Balance: $ ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: $ ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, name, balance, limit);
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                int option = 0;
+                Console.WriteLine("Choose an operation below:\n" +
+                    "1 - Deposit\n" +
+                    "2 - Withdraw");
+                Console.Write("Chosen option: ");
+                option = int.Parse(Console.ReadLine());
+                
+                
+                switch (option)
+                {
+
+                    case 1:
+                        Console.Write("Enter amount for deposit: $ ");
+                        double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        account.Deposit(amount);
+                        break;
+                    case 2:
+                        Console.Write("Enter amount for withdraw: $ ");
+                        amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        account.Withdraw(amount);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+
+                }
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error in Operation: " + e.Message);
+            }
+            
+            
+            
+
+        }
+    }
+}
